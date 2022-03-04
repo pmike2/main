679be4acf5341c57db8819ffe5c94f8ab9548fae
Beau Pierre-Michael <pierre-michael.beau@ign.fr>
2022-03-04 17:36:47 +0100

    commit

--- sandbox/sender/sender.cpp
+++ sandbox/sender/sender.cpp
@@ -8,10 +8,17 @@ https://linuxhint.com/posix-shared-memory-c-programming/
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+
 #include "../protocol.h"
+#include "keyseq.h"
+#include "utile.h"
 
 
 int main() {
+	/*
 	// create shared memory object; renvoie un file descriptor
 	// O_CREAT : crée si n'existe pas ; O_EXCL : si existe déjà, erreur ; O_RDWR : read & write
 	int fd= shm_open(NAME, O_CREAT | O_EXCL | O_RDWR, 0600);
@@ -34,6 +41,38 @@ int main() {
 
 	// fermeture file
 	close(fd);
+	*/
+
+	/*using namespace std::chrono;
+
+	system_clock::time_point tp_start= system_clock::now();
+	for (unsigned int i=0; i<10000; i++) {
+		system_clock::time_point tp= system_clock::now();
+  		//system_clock::duration dtn = tp.time_since_epoch();
+		  system_clock::duration dtn= tp- tp_start;
+		
+		std::cout << dtn.count() << "\n";
+	}*/
+
+	srand(time(NULL));
+
+	Sequence * seq= new Sequence();
+
+	for (unsigned int i=0; i<10; ++i) {
+		seq->insert_event(rand_int(1, 100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand_int(10, 200)));
+	}
+	while (true) {
+		seq->update();
+	}
+
+	/*seq->insert_event(12);
+	seq->insert_event(23);
+	seq->insert_event(23);*/
+	
+	//seq->update();
+	
+	delete seq;
 	
 	return 0;
 }
