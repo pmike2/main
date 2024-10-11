8b7dd42e14d8b7d2a2dd1cd0c763ad5ab520b016
Beau Pierre-Michael <pierre-michael.beau@ign.fr>
2024-10-11 16:34:21 +0200

    voro OK!

---
+++ libs/typedefs.h
@@ -0,0 +1,15 @@
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+
+#include <glm/glm.hpp>
+
+
+//using number = float;
+//using pt_type = glm::vec2;
+
+
+using number = double;
+using pt_type = glm::dvec2;
+
+
+#endif
