3275d6a66a6393bb6ac53b72dbd448effddd6d29
Beau Pierre-Michael <pierre-michael.beau@ign.fr>
2025-10-21 16:52:32 +0200

    audio2video

--- audio2video_2/srcs/main.cpp
+++ audio2video_2/srcs/main.cpp
@@ -19,11 +19,12 @@ int main() {
 
 	const unsigned int BLOCK_SIZE = 1024;
 	const unsigned int DELTA_OFFSET = 512;
+	const unsigned int WAV_TXT_NSAMPLES = 10000;
 
 	//std::filesystem::path wav_path = "../data/wav/sine_100.wav";
-	std::filesystem::path wav_path = "../data/wav/sine_1000.wav";
+	//std::filesystem::path wav_path = "../data/wav/sine_1000.wav";
 	//std::filesystem::path wav_path = "../data/wav/sine_noise.wav";
-	//std::filesystem::path wav_path = "../data/wav/bcl1.wav";
+	std::filesystem::path wav_path = "../data/wav/bcl6.wav";
 
 	std::filesystem::path parent_path = wav_path.parent_path() / wav_path.stem();
 	std::filesystem::path ffts_path = parent_path / "fft";
@@ -39,76 +40,75 @@ int main() {
 		std::system(cmd.c_str());
 	}
 
-	SF_INFO info;
-	SNDFILE* file = sf_open(wav_path.string().c_str(), SFM_READ, &info);
-	if (!file) {
+	SF_INFO wav_info;
+	SNDFILE* wav_file = sf_open(wav_path.string().c_str(), SFM_READ, &wav_info);
+	if (!wav_file) {
 		std::cerr << "sf_open erreur " << wav_path << "\n";
 		return 1;
+	}
+	if (wav_info.channels> 2) {
+		std::cerr << "n channels = " << wav_info.channels << " non supporté\n";
+		return 2;
 	}
 
 	std::ofstream ofs(json_path.string());
 	json js;
-	js["frames"] = info.frames;
-	js["samplerate"] = info.samplerate;
-	js["channels"] = info.channels;
+	js["frames"] = wav_info.frames;
+	js["samplerate"] = wav_info.samplerate;
+	js["channels"] = wav_info.channels;
 	js["block_size"] = BLOCK_SIZE;
 	js["delta_offset"] = DELTA_OFFSET;
-	ofs << std::setw(4) << js << "\n";
-	ofs.close();
 
-	double buff[info.channels* info.frames];
-	sf_read_double(file, buff, info.channels* info.frames);
-	sf_close(file);
-
-	double buff_mono[info.frames];
-	if (info.channels == 2) {
-		for (int i=0; i<info.frames; ++i) {
-			buff_mono[i] = buff[2 * i]; // on prend juste left
-		}
-	}
-	else if (info.channels == 1) {
-		for (int i=0; i<info.frames; ++i) {
-			buff_mono[i] = buff[i];
-		}
-	}
-	else {
-		std::cerr << "n channels = " << info.channels << " non supporté\n";
-		return 2;
-	}
-
-	std::ofstream wav_file(wav_txt_path.string());
-	for (int i=0; i<info.frames; ++i) {
-		wav_file << buff_mono[i] << "\n";
-	}
-	wav_file.close();
+	std::ofstream wav_txt(wav_txt_path.string());
+	double wav_txt_step = wav_info.frames / WAV_TXT_NSAMPLES;
 
 	fftw_complex *in, *out;
 	fftw_plan p;
 	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BLOCK_SIZE);
 	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BLOCK_SIZE);
 
-	bool done = false;
-	unsigned int offset = 0;
+	unsigned int idx_offset = 0;
+	double buff[wav_info.channels* BLOCK_SIZE];
+	double wav_txt_pos = 0.0;
 	while (true) {
-		for (unsigned int i=0; i<BLOCK_SIZE; ++i) {
-			if (offset + i>= info.frames) {
-				done = true;
+		unsigned int n_frames_read = sf_read_double(wav_file, buff, wav_info.channels* BLOCK_SIZE);
+		if (n_frames_read < wav_info.channels * BLOCK_SIZE) {
+			break;
+		}
+
+		while (true) {
+			if (wav_txt_pos < idx_offset * DELTA_OFFSET + BLOCK_SIZE) {
+				unsigned int buff_idx = (unsigned int)(wav_txt_pos) - idx_offset * DELTA_OFFSET;
+				if (wav_info.channels == 2) {
+					wav_txt << buff[2 * buff_idx] << "\n";
+				}
+				else if (wav_info.channels == 1) {
+					wav_txt << buff[buff_idx] << "\n";
+				}
+				wav_txt_pos+= wav_txt_step;
+			}
+			else {
 				break;
 			}
+		}
+
+		for (unsigned int i=0; i<BLOCK_SIZE; ++i) {
 			double hanning= 0.5* (1.0- cos(2.0* M_PI* (double)(i)/ (double)(BLOCK_SIZE)));
 
-			//in[i][0] = buff_mono[offset + i];
-			in[i][0] = buff_mono[offset + i] * hanning;
-			in[i][1] = 0.0;
-		}
-		if (done) {
-			break;
+			if (wav_info.channels == 2) {
+				in[i][0] = buff[2 * i] * hanning; // left
+				in[i][1] = 0.0;
+			}
+			else if (wav_info.channels == 1) {
+				in[i][0] = buff[i] * hanning;
+				in[i][1] = 0.0;
+			}
 		}
 
 		p = fftw_plan_dft_1d(BLOCK_SIZE, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
 		fftw_execute(p);
 		
-		std::filesystem::path fft_file_path = ffts_path / (std::to_string(offset) + ".txt");
+		std::filesystem::path fft_file_path = ffts_path / (std::to_string(idx_offset) + ".txt");
 		std::ofstream fft_file(fft_file_path.string());
 		for (unsigned int i=0; i<BLOCK_SIZE / 2; ++i) {
 			double amplitude = sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
@@ -116,8 +116,15 @@ int main() {
 		}
 		fft_file.close();
 
-		offset+= DELTA_OFFSET;
+		idx_offset++;
+		sf_seek(wav_file, idx_offset * DELTA_OFFSET, SEEK_SET);
 	}
+	js["n_blocks"] = idx_offset;
+
+	ofs << std::setw(4) << js << "\n";
+	ofs.close();
+	sf_close(wav_file);
+	wav_txt.close();
 
 	fftw_destroy_plan(p);
 	fftw_free(in);
--- audio2video_2/srcs/plot.py
+++ audio2video_2/srcs/plot.py
@@ -2,17 +2,25 @@
 
 import os
 import sys
-import math
 import json
 from pprint import pprint as pp
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+FREQ_GROUPS_LIMITS = [100.0, 300.0, 600.0, 1000.0, 2000.0, 3000.0, 7000.0, 50000.0]
+TRANSIENT_THRESHOLD = 0.5
+ENVELOPE_END_RATIO = 0.008
+MIN_DIST_BETWEEN_TRANSIENT = 20
+BAND_COLORS = ["red", "blueviolet", "blue", "darkturquoise", "green", "orange", "olive", "gold"]
+
+
 #root = "../data/wav/sine_100"
 #root = "../data/wav/sine_1000"
 #root = "../data/wav/sine_noise"
-root = "../data/wav/bcl1"
+root = "../data/wav/bcl6"
+
 
 json_path = os.path.join(root, os.path.basename(root) + ".json")
 wav_txt_path = os.path.join(root, os.path.basename(root) + ".txt")
@@ -25,6 +33,7 @@ n_samples = js["frames"]
 sample_rate = js["samplerate"]
 block_size = js["block_size"]
 delta_offset = js["delta_offset"]
+n_blocks = js["n_blocks"]
 #duration = n_samples / sample_rate
 
 with open(wav_txt_path) as f:
@@ -41,105 +50,119 @@ for i, fft_file in enumerate(fft_files):
 
 fft_data_by_freq = list(zip(*fft_data))
 
-# freq_limits = [100.0, 200.0, 300.0, 500.0, 1000.0, 2000.0, 5000.0, 20000.0]
-# fft_data_by_freq_grouped = [[]] * len(freq_limits)
-# for idx_freq, data in enumerate(fft_data_by_freq):
-# 	freq = idx_freq * sample_rate / block_size
-# 	idx_limit_ok = None
-# 	for idx_limit, freq_limit in enumerate(freq_limits):
-# 		if freq< freq_limit:
-# 			idx_limit_ok = idx_limit
-# 			break
-# 	fft_data_by_freq_grouped[idx_limit_ok]
-
 transients = {}
 for idx_freq, data in enumerate(fft_data_by_freq):
 	transients[idx_freq] = []
 	freq = idx_freq * sample_rate / block_size
-	max_data = max(data)
-	for idx in range(len(data)- 1):
-		if data[idx + 1] - data[idx] > max_data * 0.8:
-		#if abs(data[idx + 1] - data[idx]) > max_data * 0.8:
+	min_data, max_data = min(data), max(data)
+	for idx in range(n_blocks- 1):
+		if data[idx + 1] - data[idx] > (max_data - min_data) * TRANSIENT_THRESHOLD:
 			transients[idx_freq].append(idx + 1)
 
 #pp(transients)
 #sys.exit()
 
-freq_limits = [100.0, 300.0, 600.0, 1000.0, 2000.0, 3000.0, 7000.0, 50000.0]
 transients_grouped = []
-for i in range(len(freq_limits)):
+for i in range(len(FREQ_GROUPS_LIMITS)):
 	transients_grouped.append([])
 
 for idx_freq, l_idx_block in transients.items():
 	freq = idx_freq * sample_rate / block_size
 	idx_limit_ok = None
-	for idx_limit, freq_limit in enumerate(freq_limits):
-		#print(f"freq={freq} ; freq_limit={freq_limit}")
+	for idx_limit, freq_limit in enumerate(FREQ_GROUPS_LIMITS):
 		if freq< freq_limit:
 			idx_limit_ok = idx_limit
 			break
-	#print(f"idx_freq={idx_freq} ; freq={freq} ; idx_limit_ok={idx_limit_ok} b={1 in l_idx_block} ; b2={1 in transients_grouped[0]}")
 	transients_grouped[idx_limit_ok]+= l_idx_block
 
-#print(1 in transients_grouped[0])
-
 for i in range(len(transients_grouped)):
-	#print(f"i={i} ; len={len(transients_grouped[i])}")
 	transients_grouped[i] = sorted(list(set(transients_grouped[i])))
-	#print(f"i={i} ; len={len(transients_grouped[i])}")
 
 #pp(transients_grouped)
 #sys.exit()
 
-envelopes = []
-threshold = 0.1
+transients_grouped_pruned = []
+#for i in range(len(FREQ_GROUPS_LIMITS)):
+#	transients_grouped_pruned.append([])
+
 for idx_freq_group, l_block_idx_transient in enumerate(transients_grouped):
+	if len(l_block_idx_transient) == 0:
+		transients_grouped_pruned.append([])
+		continue
+	
+	l = [l_block_idx_transient[0]]
+	last_block_idx = 0
+	for i, block_idx_transient in enumerate(l_block_idx_transient):
+		if i == 0:
+			continue
+		if block_idx_transient > last_block_idx + MIN_DIST_BETWEEN_TRANSIENT:
+			last_block_idx = block_idx_transient
+			l.append(block_idx_transient)
+
+	transients_grouped_pruned.append(l)
+
+#pp(transients_grouped_pruned)
+#sys.exit()
+
+envelopes = []
+for idx_freq_group, l_block_idx_transient in enumerate(transients_grouped_pruned):
 	if idx_freq_group == 0:
 		freq_min = 0.0
 	else:
-		freq_min = freq_limits[idx_freq_group - 1]
+		freq_min = FREQ_GROUPS_LIMITS[idx_freq_group - 1]
 	
-	freq_max = freq_limits[idx_freq_group]
+	freq_max = FREQ_GROUPS_LIMITS[idx_freq_group]
 	
-	for block_idx_transient in l_block_idx_transient:
+	for i, block_idx_transient in enumerate(l_block_idx_transient):
 		d = {"idx_freq_group" : idx_freq_group, "freq_min" : freq_min, "freq_max" : freq_max, "block_idx" : block_idx_transient, "amplitudes" : []}
 
+		if i == len(l_block_idx_transient) - 1:
+			block_idx_transient_next = n_blocks
+		else:
+			block_idx_transient_next = l_block_idx_transient[i + 1]
+
 		amplitudes = []
-		for block_idx in range(block_idx_transient, len(data)):
+		for block_idx in range(block_idx_transient, block_idx_transient_next):
 			amplitudes.append(0.0)
 		
 		n_freqs = 0
+		min_data, max_data = 1e5, -1e5
 		for idx_freq, data in enumerate(fft_data_by_freq):
 			freq = idx_freq * sample_rate / block_size
 			if freq>= freq_min and freq <= freq_max:
 				n_freqs += 1
-				for block_idx in range(block_idx_transient, len(data)):
-					#print(len(amplitudes))
-					#print(len(data))
-					#print(block_idx)
-					#print("------")
+				for block_idx in range(block_idx_transient, block_idx_transient_next):
 					amplitudes[block_idx - block_idx_transient]+= data[block_idx]
+					if data[block_idx] < min_data:
+						min_data = data[block_idx]
+					if data[block_idx] > max_data:
+						max_data = data[block_idx]
 		
-		block_idx_end = len(data)
-		for block_idx in range(block_idx_transient, len(data)):
+		block_idx_end = block_idx_transient_next
+		for block_idx in range(block_idx_transient + 1, block_idx_transient_next):
 			amplitudes[block_idx - block_idx_transient] /= n_freqs
-			if amplitudes[block_idx - block_idx_transient] < threshold:
-				block_idx_end = block_idx - block_idx_transient
+			if amplitudes[block_idx - block_idx_transient] < min_data + (max_data - min_data) * ENVELOPE_END_RATIO:
+				block_idx_end = block_idx
 				break
 		
-		d["amplitudes"] = amplitudes[:block_idx_end]
+		d["amplitudes"] = amplitudes[:block_idx_end - block_idx_transient]
 
 		envelopes.append(d)
 
 #pp(envelopes)
 #sys.exit()
 
+js["envelopes"] = envelopes
+with open(json_path, "w") as f:
+	json.dump(js, f)
+
 fig, ax = plt.subplots()
 fig.set_size_inches(20.0, 8.0)
 
-x_wav = np.linspace(0.0, n_samples, n_samples)
+#x_wav = np.linspace(0.0, n_samples, n_samples)
+x_wav = np.linspace(0.0, n_samples, len(wav_data))
 y_wav = np.array(wav_data, dtype='float32')
-ax.plot(x_wav, y_wav, linewidth=0.2, label=f'wav')
+ax.plot(x_wav, y_wav, linewidth=0.4, label=f'wav', c='lightgrey')
 
 #x_fft = np.array([block_size/ 2+ i * delta_offset for i in range(len(fft_data_by_freq[0]))], dtype='float32')
 #l_freqs = [500.0, 5000.0]
@@ -151,17 +174,15 @@ ax.plot(x_wav, y_wav, linewidth=0.2, label=f'wav')
 	#y_fft = np.log(np.array(fft_data_by_freq[idx_freq], dtype='float32'))
 #	ax.plot(x_fft, y_fft, linewidth=0.5, label=f'{freq} Hz')
 
-#l = [[block_size/ 2+ x * delta_offset, y * 0.001] for y in transients_grouped.keys() for x in transients_grouped[y]]
-#l = [[block_size/ 2+ x * delta_offset, y * 0.1] for y in range(len(transients_grouped)) for x in transients_grouped[y]]
-#x_transients = np.array(list(zip(*l))[0], dtype='float32')
-#y_transients = np.array(list(zip(*l))[1], dtype='float32')
-#ax.scatter(x_transients, y_transients, s=2.0, c='r')
+l = [[block_size/ 2+ x * delta_offset, y * 0.1] for y in range(len(transients_grouped_pruned)) for x in transients_grouped_pruned[y]]
+x_transients = np.array(list(zip(*l))[0], dtype='float32')
+y_transients = np.array(list(zip(*l))[1], dtype='float32')
+ax.scatter(x_transients, y_transients, s=2.0, c='r')
 
-colors = ["red", "blueviolet", "blue", "darkturquoise", "green", "orange", "yellow", "ivory"]
 for envelope in envelopes:
 	x_env = np.array([block_size/ 2+ delta_offset * (envelope["block_idx"] + i) for i in range(len(envelope["amplitudes"]))], dtype='float32')
 	y_env = np.array(envelope["amplitudes"], dtype='float32') / np.max(envelope["amplitudes"])
-	ax.plot(x_env, y_env, linewidth=0.5, c=colors[envelope["idx_freq_group"]])
+	ax.plot(x_env, y_env, linewidth=0.5, c=BAND_COLORS[envelope["idx_freq_group"]])
 
 ax.legend()
 
