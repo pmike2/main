d88da050ec2eec76bd649a83282fe8ae517cb2ed
Beau Pierre-Michael <pierre-michael.beau@ign.fr>
2025-10-21 13:09:51 +0200

    audio2video

--- audio2video_2/srcs/main.cpp
+++ audio2video_2/srcs/main.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <string>
 #include <cmath>
+#include <filesystem>
 
 #include <fftw3.h>
 #include "sndfile.h"
@@ -19,21 +20,33 @@ int main() {
 	const unsigned int BLOCK_SIZE = 1024;
 	const unsigned int DELTA_OFFSET = 512;
 
-	//std::string wav_path = "../data/wav/sine_noise.wav";
-	std::string wav_path = "../data/wav/test.wav";
-	//std::string wav_path = "../data/wav/sine_100.wav";
-	//std::string wav_path = "../data/wav/sine_1000.wav";
+	//std::filesystem::path wav_path = "../data/wav/sine_100.wav";
+	std::filesystem::path wav_path = "../data/wav/sine_1000.wav";
+	//std::filesystem::path wav_path = "../data/wav/sine_noise.wav";
+	//std::filesystem::path wav_path = "../data/wav/bcl1.wav";
 
+	std::filesystem::path parent_path = wav_path.parent_path() / wav_path.stem();
+	std::filesystem::path ffts_path = parent_path / "fft";
+	std::filesystem::path json_path = parent_path / (wav_path.stem().string() + ".json");
+	std::filesystem::path wav_txt_path = parent_path / (wav_path.stem().string() + ".txt");
+
+	std::vector<std::string> cmds = {
+		"rm -rf " + parent_path.string(),
+		"mkdir " + parent_path.string(),
+		"mkdir " + ffts_path.string(),
+	};
+	for (auto cmd : cmds) {
+		std::system(cmd.c_str());
+	}
 
 	SF_INFO info;
-	SNDFILE* file = sf_open(wav_path.c_str(), SFM_READ, &info);
+	SNDFILE* file = sf_open(wav_path.string().c_str(), SFM_READ, &info);
 	if (!file) {
 		std::cerr << "sf_open erreur " << wav_path << "\n";
 		return 1;
 	}
 
-	std::string json_path = "../data/wav/test.json";
-	std::ofstream ofs(json_path);
+	std::ofstream ofs(json_path.string());
 	json js;
 	js["frames"] = info.frames;
 	js["samplerate"] = info.samplerate;
@@ -63,7 +76,7 @@ int main() {
 		return 2;
 	}
 
-	std::ofstream wav_file("../data/wav/test.txt");
+	std::ofstream wav_file(wav_txt_path.string());
 	for (int i=0; i<info.frames; ++i) {
 		wav_file << buff_mono[i] << "\n";
 	}
@@ -73,8 +86,6 @@ int main() {
 	fftw_plan p;
 	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BLOCK_SIZE);
 	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BLOCK_SIZE);
-
-	std::system("rm ../data/fft/test*");
 
 	bool done = false;
 	unsigned int offset = 0;
@@ -97,8 +108,8 @@ int main() {
 		p = fftw_plan_dft_1d(BLOCK_SIZE, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
 		fftw_execute(p);
 		
-		std::string fft_file_path = "../data/fft/test_" + std::to_string(offset) + ".txt";
-		std::ofstream fft_file(fft_file_path);
+		std::filesystem::path fft_file_path = ffts_path / (std::to_string(offset) + ".txt");
+		std::ofstream fft_file(fft_file_path.string());
 		for (unsigned int i=0; i<BLOCK_SIZE / 2; ++i) {
 			double amplitude = sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
 			fft_file << amplitude << "\n";
--- audio2video_2/srcs/plot.py
+++ audio2video_2/srcs/plot.py
@@ -9,8 +9,16 @@ from pprint import pprint as pp
 import matplotlib.pyplot as plt
 import numpy as np
 
+#root = "../data/wav/sine_100"
+#root = "../data/wav/sine_1000"
+#root = "../data/wav/sine_noise"
+root = "../data/wav/bcl1"
 
-with open("../data/wav/test.json") as f:
+json_path = os.path.join(root, os.path.basename(root) + ".json")
+wav_txt_path = os.path.join(root, os.path.basename(root) + ".txt")
+root_fft = os.path.join(root, "fft")
+
+with open(json_path) as f:
 	js = json.load(f)
 
 n_samples = js["frames"]
@@ -19,13 +27,12 @@ block_size = js["block_size"]
 delta_offset = js["delta_offset"]
 #duration = n_samples / sample_rate
 
-with open("../data/wav/test.txt") as f:
+with open(wav_txt_path) as f:
 	b = f.readlines()
 wav_data = [float(x.strip()) for x in b if x.strip()]
 
-root_fft = "../data/fft"
 fft_files = [os.path.join(root_fft, x) for x in os.listdir(root_fft) if os.path.splitext(x)[1] == ".txt" and not x.startswith(".")]
-fft_files.sort(key=lambda x : int(os.path.splitext(os.path.basename(x))[0].split("_")[1]))
+fft_files.sort(key=lambda x : int(os.path.splitext(os.path.basename(x))[0]))
 fft_data = []
 for i, fft_file in enumerate(fft_files):
 	with open(fft_file) as f:
@@ -81,7 +88,50 @@ for i in range(len(transients_grouped)):
 	transients_grouped[i] = sorted(list(set(transients_grouped[i])))
 	#print(f"i={i} ; len={len(transients_grouped[i])}")
 
-pp(transients_grouped)
+#pp(transients_grouped)
+#sys.exit()
+
+envelopes = []
+threshold = 0.1
+for idx_freq_group, l_block_idx_transient in enumerate(transients_grouped):
+	if idx_freq_group == 0:
+		freq_min = 0.0
+	else:
+		freq_min = freq_limits[idx_freq_group - 1]
+	
+	freq_max = freq_limits[idx_freq_group]
+	
+	for block_idx_transient in l_block_idx_transient:
+		d = {"idx_freq_group" : idx_freq_group, "freq_min" : freq_min, "freq_max" : freq_max, "block_idx" : block_idx_transient, "amplitudes" : []}
+
+		amplitudes = []
+		for block_idx in range(block_idx_transient, len(data)):
+			amplitudes.append(0.0)
+		
+		n_freqs = 0
+		for idx_freq, data in enumerate(fft_data_by_freq):
+			freq = idx_freq * sample_rate / block_size
+			if freq>= freq_min and freq <= freq_max:
+				n_freqs += 1
+				for block_idx in range(block_idx_transient, len(data)):
+					#print(len(amplitudes))
+					#print(len(data))
+					#print(block_idx)
+					#print("------")
+					amplitudes[block_idx - block_idx_transient]+= data[block_idx]
+		
+		block_idx_end = len(data)
+		for block_idx in range(block_idx_transient, len(data)):
+			amplitudes[block_idx - block_idx_transient] /= n_freqs
+			if amplitudes[block_idx - block_idx_transient] < threshold:
+				block_idx_end = block_idx - block_idx_transient
+				break
+		
+		d["amplitudes"] = amplitudes[:block_idx_end]
+
+		envelopes.append(d)
+
+#pp(envelopes)
 #sys.exit()
 
 fig, ax = plt.subplots()
@@ -91,23 +141,27 @@ x_wav = np.linspace(0.0, n_samples, n_samples)
 y_wav = np.array(wav_data, dtype='float32')
 ax.plot(x_wav, y_wav, linewidth=0.2, label=f'wav')
 
-x_fft = np.array([block_size/ 2+ i * delta_offset for i in range(len(fft_data_by_freq[0]))], dtype='float32')
-l_freqs = [500.0, 5000.0]
-for freq in l_freqs:
-	idx_freq = int(math.ceil(freq * float(block_size) / float(sample_rate)))
-	print(f"freq={freq} ; idx_freq={idx_freq}")
-	y_fft = np.array(fft_data_by_freq[idx_freq], dtype='float32') * 0.001 * idx_freq
+#x_fft = np.array([block_size/ 2+ i * delta_offset for i in range(len(fft_data_by_freq[0]))], dtype='float32')
+#l_freqs = [500.0, 5000.0]
+#for freq in l_freqs:
+#	idx_freq = int(math.ceil(freq * float(block_size) / float(sample_rate)))
+	#print(f"freq={freq} ; idx_freq={idx_freq}")
+#	y_fft = np.array(fft_data_by_freq[idx_freq], dtype='float32') * 0.001 * idx_freq
 	#y_fft = np.array(fft_data_by_freq[idx_freq], dtype='float32') / np.max(fft_data_by_freq[idx_freq])
 	#y_fft = np.log(np.array(fft_data_by_freq[idx_freq], dtype='float32'))
-	ax.plot(x_fft, y_fft, linewidth=0.5, label=f'{freq} Hz')
+#	ax.plot(x_fft, y_fft, linewidth=0.5, label=f'{freq} Hz')
 
 #l = [[block_size/ 2+ x * delta_offset, y * 0.001] for y in transients_grouped.keys() for x in transients_grouped[y]]
+#l = [[block_size/ 2+ x * delta_offset, y * 0.1] for y in range(len(transients_grouped)) for x in transients_grouped[y]]
+#x_transients = np.array(list(zip(*l))[0], dtype='float32')
+#y_transients = np.array(list(zip(*l))[1], dtype='float32')
+#ax.scatter(x_transients, y_transients, s=2.0, c='r')
 
-l = [[block_size/ 2+ x * delta_offset, y * 0.1] for y in range(len(transients_grouped)) for x in transients_grouped[y]]
-
-x_transients = np.array(list(zip(*l))[0], dtype='float32')
-y_transients = np.array(list(zip(*l))[1], dtype='float32')
-ax.scatter(x_transients, y_transients, s=2.0, c='r')
+colors = ["red", "blueviolet", "blue", "darkturquoise", "green", "orange", "yellow", "ivory"]
+for envelope in envelopes:
+	x_env = np.array([block_size/ 2+ delta_offset * (envelope["block_idx"] + i) for i in range(len(envelope["amplitudes"]))], dtype='float32')
+	y_env = np.array(envelope["amplitudes"], dtype='float32') / np.max(envelope["amplitudes"])
+	ax.plot(x_env, y_env, linewidth=0.5, c=colors[envelope["idx_freq_group"]])
 
 ax.legend()
 
